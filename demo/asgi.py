ff72dce8503e3bd1ebd013520b2ca046effc949c
ank <ank@gmail.com>
2024-09-11 17:00:00 +0530

    test1 changes

--- demo/asgi.py
+++ demo/asgi.py
@@ -6,7 +6,7 @@ It exposes the ASGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
 """
-
+print("new branch test1")
 import os
 
 from django.core.asgi import get_asgi_application
