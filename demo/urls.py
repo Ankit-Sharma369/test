ca59478291f6e90b81f0c197e8035218be7acedc
ank <ank@gmail.com>
2024-09-12 14:56:33 +0530

    all file

--- demo/urls.py
+++ demo/urls.py
@@ -1,6 +1,20 @@
-from django.contrib import admin
-from django.urls import path
+import math
 
-urlpatterns = [
-    path('admin/', admin.site.urls),
-]
+def lcm (a,b):
+    return abs(a*b) // math.gcd(a,b)
+
+
+a = 25
+b = 10
+print(lcm(a,b))
+
+
+
+def hcf(a,b):
+    while b!=0:
+        a,b =b, a%b
+    return a
+
+print(hcf(a,b))
+
+
